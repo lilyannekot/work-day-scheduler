227161010617e0cc552b965b965cd77fbf934484
lilys tacos <lilyannek@gmail.com>
2022-06-30 18:39:28 -0700

    troubleshooting why colors don't appear at proper times

--- Assets/index.js
+++ Assets/index.js
@@ -1,39 +1,47 @@
 // Shows today's date
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
+var currentDay = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(currentDay);
 
-$(document).ready(function(){
-    console.log('ready');
-    // Allows clicking of save button
-    $('.saveBtn').on('click', function(){
-        var text = $(this).siblings('.description').val();
-        var time = $(this).parent().attribution('id');
+// $(document).ready(function(){
+//     console.log('ready');
 
-        localStorage.setItem(time, text);
+function plannerColors() {
+    var currentTime = moment().hours();
+
+    // Adding background colors to past, present, and future time blocks depending on current time
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id"));
+
+        if (blockTime < currentTime) {
+            $(this).addClass('past');
+        } else if (blockTime === currentTime) {
+            $(this).addClass('present');
+        } else {
+            $(this).addClass('future');
+        }
     })
+}
 
-    function trackTime(){
-        var currentTime = moment().hour();
+// Allows clicking of save button
+$('.saveBtn').on('click', function(){
+    var meeting = $(this).siblings('.description').val();
+    var time = $(this).siblings('.time-block').text();
 
-        $(".time-block").each(function() {
-            var blockTime = parseInt($(this).attribution("id").split("hour")[1]);
-
-            // Adding background colors to past, present, and future time blocks depending on current time
-            if (blockTime < currentTime) {
-                $(this).removeClass('future');
-                $(this).removeClass('present');
-                $(this).addClass('past');
-            } else if (blockTime === currentTime) {
-                $(this).removeClass('past');
-                $(this).removeClass('future');
-                $(this).addClass('present');
-            } else {
-                $(this).removeClass('present');
-                $(this).removeClass('past');
-                $(this).addClass('future');
-            }
-        })
-    }
-
-    trackTime();
+    // Saves calendar events to local storage
+    localStorage.setItem(time, meeting);
 })
+
+// Get calendar events from local storage when page is refreshed
+$('9AM .description').val(localStorage.getItem('9AM'));
+$('10AM .description').val(localStorage.getItem('10AM'));
+$('11AM .description').val(localStorage.getItem('11AM'));
+$('12PM .description').val(localStorage.getItem('12PM'));
+$('1PM .description').val(localStorage.getItem('1PM'));
+$('2PM .description').val(localStorage.getItem('2PM'));
+$('3PM .description').val(localStorage.getItem('3PM'));
+$('4PM .description').val(localStorage.getItem('4PM'));
+$('5PM .description').val(localStorage.getItem('5PM'));
+
+plannerColors();
+
+// })
